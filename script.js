028f3614942c7de3955ef7cd2b8f3c4ab1e36603
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-07-27 20:12:20 -0500

    added QA feedback function.

--- script.js
+++ script.js
@@ -63,4 +63,43 @@ submitBtn.addEventListener("click", function(event) {
     window.location.href = "highscores.html"
 });
 
+function addScore() {
+    userNameInput = document.getElementById("userInitials").value
+
+    var newScore = {
+        name: userNameInput,
+        score: secondsLeft
+    };
+
+    var highScores = JSON.parse(localStorage.getItem("highScores") || "[]");
+    highScores.push(newScore)
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+}
+
+function hideFeedback() {
+    var pElement = document.getElementsByClassName("feedback")[0]
+    pElement.style.display="none"
+}
+
+function showFeedback() {
+    var pElement = document.getElementsByClassName("feedback")[0]
+    pElement.removeAttribute("style");
+}
+
+answerChoices.addEventListener("click", function (event) {
+    var pElement = document.getElementsByClassName("feedback")[0]
+
+    if (answer === event.target.textContent) {
+        pElement.innerHTML = "Correct!";
+        setTimeout(hideFeedback,1225);
+        showFeedback();
+    }
+    else {
+        pElement.innerHTML = "Incorrect.";
+        setTimeout(hideFeedback,1225);
+        secondsLeft = secondsLeft - 20;
+        showFeedback();
+        }
+        makeQuestions();
+});
 
